a7363766df0d12814e47b9eb9c5045b1a3630cca
arthur-menezes202 <arthurmcorrea282@gmail.com>
2022-02-11 11:10:09 -0300

    adicionando bootstrap e mudando o title

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <button type="button" className="btn btn-primary">Primary</button>
+      
+    </div>
+  );
+}
+
+export default App;
