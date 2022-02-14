ce4d8be81a20309cb0a41bbc1b3d4e83100e4a28
arthur-menezes202 <arthurmcorrea282@gmail.com>
2022-02-14 15:41:12 -0300

    teste para realizar um feth na api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+const [pokemom, setPokemom] = useState(0);
+
+
+async function getPerson() {
+  const pokemom = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  .then((response) => response.json())
+  .then((json) => setPokemom(json));
+  console.log(pokemom);
+}
+
   return (
     <div className="">
-      <button type="button" className="btn btn-primary">Primary</button>
-      
+      <button type="button" className="btn btn-primary" onClick={getPerson}>Primary</button>
+      <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg' />
     </div>
   );
 }
