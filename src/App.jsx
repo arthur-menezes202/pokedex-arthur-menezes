c262c0c1bfea7ae2df9a7997b34d56e541f675a1
arthur-menezes202 <arthurmcorrea282@gmail.com>
2022-02-21 14:08:00 -0300

    testes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const [pokemom, setPokemom] = useState(0);
 
 
 async function getPerson() {
-  const pokemom = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  const pokemom =  fetch('https://pokeapi.co/api/v2/pokemon/ditto')
   .then((response) => response.json())
   .then((json) => setPokemom(json));
   console.log(pokemom);
@@ -18,7 +18,8 @@ async function getPerson() {
   return (
     <div className="">
       <button type="button" className="btn btn-primary" onClick={getPerson}>Primary</button>
-      <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg' />
+      <button type="button" className="btn btn-primary" onClick={getPerson}>pokemon</button>
+
     </div>
   );
 }
